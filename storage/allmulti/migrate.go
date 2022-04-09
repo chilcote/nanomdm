5142ae3c22d59617b6793a13592701be0231d239
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-09 14:45:24 +0800

    Delete storage directory

--- storage/allmulti/migrate.go
+++
@@ -1,8 +0,0 @@
-package allmulti
-
-import "context"
-
-func (ms *MultiAllStorage) RetrieveMigrationCheckins(ctx context.Context, c chan<- interface{}) error {
-	ms.logger.Info("msg", "only using first store for migration")
-	return ms.stores[0].RetrieveMigrationCheckins(ctx, c)
-}
